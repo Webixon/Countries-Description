f02599b554ad4940a0406a3dba7b0adc99b4c2c1
Tomek Sośniewski <sosna86@gmail.com>
2022-09-03 15:13:51 +0200

    added template creation

--- main.js
+++ main.js
@@ -1,23 +1,52 @@
 let searchBtn = document.querySelector('#searchBtn')
 let inputValue = document.querySelector('#countryInput')
+let countryTicket = document.querySelector('.country');
+let globalData = null;
 let result = null;
-
+let country = null;
 
 function search (e) {
     result = e.target.value
     // console.log(result)
     lookForTheCountry()
 }
+
+function createTemplate () {
+
+    country = `<div id="flag"></div>
+                <div id="countryName">${globalData[0].name.common}</div>
+                <div id="population">${globalData[0].population}</div>
+                <div id="region">${globalData[0].region}</div>
+                <div id="capital">${globalData[0].capital}</div>`
+      console.log(globalData[0].name.common)          
+}
+
+
 inputValue.addEventListener('input', search)
 
-
-
-
+function createElement () {
+    let newEl = document.createElement('div')
+    
+    newEl.innerHTML=country;
+    newEl.classList.add()
+    newEl.style.backgroundImage = `url(${globalData[0].flags.png})`
+    newEl.style.backgroundsize = 'cover';
+    newEl.style.backgroundRepeat = 'no-repeat';
+    // newEl.style.maxHeight = '200px';
+    // newEl.style.backgroundColor = 'blue'
+    countryTicket.appendChild(newEl)
+    
+}
 
 
 function lookForTheCountry (){
 fetch(`https://restcountries.com/v3.1/name/${result}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    // .then(data => countryName = data[0].name.common)
+    // .then(data => console.log(data))
+    .then(data => globalData = data)
     .catch(err => console.log(err))
 }
+
+searchBtn.addEventListener('click', createTemplate)
+searchBtn.addEventListener('click', createElement)

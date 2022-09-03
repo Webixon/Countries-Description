38d3d9f7c99d8909fc7e8f119b92a162817ecdf0
Tomek Sośniewski <sosna86@gmail.com>
2022-09-03 16:16:00 +0200

    added creating all countries at the start

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+document.addEventListener('DOMContentLoaded', allCountries)
 let searchBtn = document.querySelector('#searchBtn')
 let inputValue = document.querySelector('#countryInput')
 let countryTicket = document.querySelector('.country');
+let container = document.querySelector('#container')
 let globalData = null;
 let result = null;
 let country = null;
@@ -47,6 +49,36 @@ fetch(`https://restcountries.com/v3.1/name/${result}`)
     .then(data => globalData = data)
     .catch(err => console.log(err))
 }
+
+
+let all = null
+function allCountries () {
+    fetch('https://restcountries.com/v3.1/all').then(res=>res.json()).then(data=>all = data)
+}
+
+
+
+function showAllCountries () {
+all.forEach(el => {
+    let fl = el.flags.png
+    let newestEl = document.createElement('div');
+    el = `<div id="flag"></div>
+    <div id="countryName">${el.name.common}</div>
+    <div id="population">${el.population}</div>
+    <div id="region">${el.region}</div>
+    <div id="capital">${el.capital}</div>`
+    newestEl.innerHTML = el;
+    newestEl.style.backgroundImage = `url(${fl})`
+    newestEl.style.backgroundsize = 'cover';
+    newestEl.style.backgroundRepeat = 'no-repeat';
+    newestEl.style.border =  '1px solid black';
+    newestEl.classList.add('country')
+    container.appendChild(newestEl)
+})}
+
+
+
+
 
 searchBtn.addEventListener('click', createTemplate)
 searchBtn.addEventListener('click', createElement)
